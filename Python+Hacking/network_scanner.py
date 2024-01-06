170c345702f321cd573dd42b97288096804b7651
GAURAV <gaurav1993rajput@gmail.com>
2024-01-06 05:16:04 -0500

    Initial commit 1

--- Python+Hacking/network_scanner.py
+++ Python+Hacking/network_scanner.py
@@ -4,7 +4,16 @@ from scapy.all import *
 
 
 def scan(ip):
-    scapy.layers.l2.arping(ip)
+    arp_request = scapy.layers.l2.ARP(pdst=ip)
+    arp_request.show()
+    broadcast = scapy.layers.l2.Ether(dst='ff:ff:ff:ff:ff:ff')
+    broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
+    # scapy.layers.l2.arping(ip)
 
 
 scan("192.168.159.1/24")
+
+# TODO: scapy.ls(scapy.layers.l2.ARP(pdst=ip))
