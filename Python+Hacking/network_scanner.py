7c9e7955e7ede5d0f59d24884627d0cd56f1bffc
GAURAV <gaurav1993rajput@gmail.com>
2024-01-10 01:17:13 -0500

    Take only the answer value

--- Python+Hacking/network_scanner.py
+++ Python+Hacking/network_scanner.py
@@ -7,10 +7,11 @@ def scan(ip):
     arp_request = scapy.layers.l2.ARP(pdst=ip)
     broadcast = scapy.layers.l2.Ether(dst='ff:ff:ff:ff:ff:ff')
     arp_request_broadcast = broadcast/arp_request
-    answered, unasnwered = scapy.layers.l2.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
-    print(unasnwered.summary())
-
+    answered_list = scapy.layers.l2.srp(arp_request_broadcast, timeout=1)[0]
+    for element in answered_list:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("---------------------------------------------------------------")
 
 scan("192.168.159.1/24")
 
